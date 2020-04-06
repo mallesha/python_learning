133c980ad2d3bf4470121dedb6aafc52dde254f1
mallesha <mallesha666@gmail.com>
2020-04-06 12:44:32 +0200

    print added at each level of dictionary to print the key value pairs of dictionary

--- Dict_for_loop.py
+++ Dict_for_loop.py
@@ -1,5 +1,21 @@
 import  json
 
+####################Basic examples ###########################
+mylist = [1,2,3,4,5,6,7,8,9,10]
+list_sum = 0
+for num in mylist:
+    #print(list_sum)
+    list_sum = list_sum + num
+print(list_sum)
+print('\n')
+
+my_list = [(1,2), (3,4), (5,6), (7,8)]
+for item in my_list:
+    print(item)
+print('\n')
+
+#############################################################
+
 device = "T02-ntsm-ci-ios38"
 hostname = "test123-hostname"
 banner = "test123-banner"
@@ -13,6 +29,7 @@ x={
   ]
 }
 
+print( "real time example \n ")
 for test in x["hname-banner-rfs:hname-banner-rfs"]:
     print(test)
     print('\n')
